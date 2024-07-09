7eb7aef4404a2e283ff2102eff50b49fc95e402a
Brijesh Kumar <brijesh_9_nov@yahoo.co.in>
2024-07-09 17:34:33 +0530

    initial commit

---
+++ Sum.java
@@ -0,0 +1,9 @@
+public class Sum {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 400;
+        int c = 0;
+        c = a+b;
+        System.out.println(c);
+    }
+}
